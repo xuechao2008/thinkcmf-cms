bcfd60ae5f45988407c9dfa21460417f1c705213
xuechao2008 <2196537621@qq.com>
2018-05-07 17:19:56 +0800

    add file public/static/images  add img call.png copyright.png email.png qr.jpg themes/ziding1/public/assets/css add css file  pagecss.css
    edit file (themes/ziding1/public/)
    footer.html head.html

---
+++ public/phpinfo.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/3
+ * Time: 9:24
+ */
+phpinfo();
--- simplewind/cmf/controller/HomeBaseController.php
+++ simplewind/cmf/controller/HomeBaseController.php
@@ -84,7 +84,7 @@ class HomeBaseController extends BaseController
         $template = $this->parseTemplate($template);
         $more     = $this->getThemeFileMore($template);
         $this->assign('theme_vars', $more['vars']);
-        dump($more);
+        /*dump($more);*/
         $this->assign('theme_widgets', $more['widgets']);
         return parent::fetch($template, $vars, $replace, $config);
     }
